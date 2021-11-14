f9161f1254a1783cac1478a06f97270a63acc708
ezzcodeezzlife <fabian.stehle@informatik.hs-fulda.de>
2021-11-14 19:55:26 +0100

    clean up arguments

--- backend/src/routes/IndexRouter.ts
+++ backend/src/routes/IndexRouter.ts
@@ -2,11 +2,11 @@ import express from "express";
 
 let router : express.Router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: express.Request, res: express.Response) => {
     res.send(getContent());
 });
 
-router.get('/index', (req, res) => {
+router.get('/index', (req: express.Request, res: express.Response) => {
     res.send(getContent());
 })
 
